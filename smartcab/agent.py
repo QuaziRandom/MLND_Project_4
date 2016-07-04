c02262e3e66945ecf951507a52d18e1064cd4b04
Aravind Battaje <aravind@oxidification.com>
2016-07-04 06:41:13 +0530

    Implement basic Q-Learning
    Create a new Q-Learner class encapsulating update() and optimal_action() methods. The class is flexible enough to allow different kinds of state and action space.
    
    Also includes a rudimentary exploration decay factor.
    
    The agent (or state space) still needs to be tweaked because it is finding high rewards in going around in circles --> Ex: +2 +2 +2 -0.5.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -3,18 +3,86 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+class QLearner(object):
+    """TODO: Fill this later."""
+
+    discount_factor = 0.3
+
+    def __init__(self, state_space, action_space):
+        self.state_space = state_space
+        self.action_space = action_space
+        self.qtable = dict()
+
+        # Q-Learner needs to span how many states and actions?
+        total_num_states = 1
+        for attribute in self.state_space.iterkeys():
+            total_num_states *= len(self.state_space[attribute])
+        print "Q-Learner has {} possible states and {} actions".format(total_num_states, len(self.action_space))
+
+    def optimal_action(self, state):
+        q = []
+        for action in self.action_space:
+            qtable_key = self.tuplize(state, action)
+            if qtable_key not in self.qtable:
+                return random.choice(self.action_space)
+            else:
+                q.append(self.qtable[qtable_key])
+        
+        return self.action_space[q.index(max(q))]
+
+    def update(self, state, action, reward, next_state, learning_rate):
+        assert learning_rate >=0 and learning_rate <= 1, "Invalid learning rate"
+        
+        qtable_key = self.tuplize(state, action)
+        if qtable_key not in self.qtable:
+            self.qtable[qtable_key] = 0
+        
+        q_prime = []
+        for act in self.action_space:
+            qtable_key_tmp = self.tuplize(next_state, act)
+            if qtable_key_tmp not in self.qtable:
+                self.qtable[qtable_key_tmp] = 0
+            q_prime.append(self.qtable[qtable_key_tmp])
+        
+        tmp = reward + self.discount_factor * max(q_prime)
+        self.qtable[qtable_key] = learning_rate * tmp + (1 - learning_rate) * self.qtable[qtable_key]
+
+    def tuplize(self, state, action):
+        return_value = [state[x] for x in sorted(state)]
+        return_value.append(action)
+        return tuple(return_value)
+
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
+
+    allowed_actions = [None, 'forward', 'left', 'right']
+    state_space = {
+        'next_waypoint': allowed_actions, 
+        'oncoming': allowed_actions,
+        'left': allowed_actions,
+        'right': allowed_actions,
+        'light': ['green', 'red']
+    }
 
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
+        
+        # Initialize additional variables here
+        self.qlearner = QLearner(self.state_space, self.allowed_actions)
+        self.prev_state = None
+        self.state = None
+        self.cumulative_reward = 0
+        self.exploration_factor = 1.0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+        # Prepare for a new trip; reset any variables here, if required
+        self.prev_state = None
+        self.state = None
+        self.cumulative_reward = 0
+        self.exploration_factor *= 0.9
 
     def update(self, t):
         # Gather inputs
@@ -22,18 +90,33 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # TODO: Update state
+        # Update state
+        self.state = {
+            'next_waypoint': self.next_waypoint, 
+            'oncoming': inputs['oncoming'],
+            'left': inputs['left'],
+            'right': inputs['right'],
+            'light': inputs['light']
+        }
         
-        # TODO: Select action according to your policy
-        actions = [None, 'forward', 'left', 'right']
-        action = random.choice(actions)
+        # Select action according to your policy
+        if random.random() > self.exploration_factor:
+            action = self.qlearner.optimal_action(self.state)
+        else:
+            action = random.choice(self.allowed_actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # TODO: Learn policy based on state, action, reward
+        # Learn policy based on state, action, reward
+        if self.prev_state is not None:
+            self.qlearner.update(self.prev_state, action, reward, self.state, 1./t)
+        
+        self.prev_state = self.state
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        self.cumulative_reward += reward
+        #print "Cumulative reward is {}".format(self.cumulative_reward)
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
 def run():
@@ -42,11 +125,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.001, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
