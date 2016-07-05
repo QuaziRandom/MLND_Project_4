732c590df464579b331565ade3414a640d5c3308
Aravind Battaje <aravind@oxidification.com>
2016-07-05 21:00:19 +0530

    Make a working agent (proper one)
    Implement such that the two states used to
    update Q-table are the one corresponding
    to before and after taking action in the same
    iteration. This is more intuitive than the previous
    version. Agent learns to reach destination
    reliably after 10 trials.
    
    Also, beautify the implementation a bit and make
    the discount factor (gamma) and exploration
    factor (epsilon) parameterizable.
    
    Parameter tuning, and potentially update
    of agent with optimal parameters pending.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -4,13 +4,15 @@ from planner import RoutePlanner
 from simulator import Simulator
 
 class QLearner(object):
-    """TODO: Fill this later."""
+    """ 
+    A (rudimentary) general Q-Learner class that implements
+    Q-learning on arbitrary state and action space.
+    """
 
-    discount_factor = 0.8
-
-    def __init__(self, state_space, action_space):
+    def __init__(self, state_space, action_space, discount_factor):
         self.state_space = state_space
         self.action_space = action_space
+        self.discount_factor = discount_factor
         self.qtable = dict()
 
         # Q-Learner needs to span how many states and actions?
@@ -64,42 +66,40 @@ class LearningAgent(Agent):
         'light': ['green', 'red']
     }
 
-    def __init__(self, env):
+    def __init__(self, env, exploration_factor=0.8, discount_factor=0.8):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         
         # Initialize additional variables here
-        self.qlearner = QLearner(self.state_space, self.allowed_actions)
-        self.prev_state = None
-        self.state = None
+        self.exploration_factor = exploration_factor
+        self.qlearner = QLearner(self.state_space, self.allowed_actions, discount_factor)
         self.cumulative_reward = 0
-        self.exploration_factor = 1.0
+        self.exploration_amount = 1.0 # Starts with 1.0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
+
         # Prepare for a new trip; reset any variables here, if required
-        self.prev_state = None
-        self.state = None
         self.cumulative_reward = 0
-        self.exploration_factor *= 0.9
+        self.exploration_amount *= self.exploration_factor # Decays exponentially every iteration
 
     def update(self, t):
         # Gather inputs
-        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # Update state
+        # Update state before taking action
         self.state = {
-            'next_waypoint': self.next_waypoint, 
+            'next_waypoint': self.next_waypoint,
             'oncoming': inputs['oncoming'],
             'left': inputs['left'],
             'light': inputs['light']
         }
         
-        # Select action according to your policy
-        if random.random() > self.exploration_factor:
+        # Select action according to policy that has decaying exploration
+        if random.random() > self.exploration_amount:
             action = self.qlearner.optimal_action(self.state)
         else:
             action = random.choice(self.allowed_actions)
@@ -107,15 +107,23 @@ class LearningAgent(Agent):
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # Learn policy based on state, action, reward
-        if self.prev_state is not None:
-            self.qlearner.update(self.state, action, reward, self.prev_state, 1./t)
-        
-        self.prev_state = self.state
+        # Update state after taking action
+        next_waypoint_after_action = self.planner.next_waypoint()
+        inputs_after_action = self.env.sense(self)
+        state_after_action = {
+            'next_waypoint': next_waypoint_after_action,
+            'oncoming': inputs_after_action['oncoming'],
+            'left': inputs_after_action['left'],
+            'light': inputs_after_action['light']
+        }
 
+        # Learn policy based on state, action, reward
+        self.qlearner.update(self.state, action, reward, state_after_action, 1./(t+1))
+        
+        # Update other variables for debug
         self.cumulative_reward += reward
-        #print "Cumulative reward is {}".format(self.cumulative_reward)
-        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
 def run():
