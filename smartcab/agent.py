58002f0da20d1880976d5cdf501b4df8a2b5a528
Aravind Battaje <aravind@oxidification.com>
2016-07-07 18:24:54 +0530

    Add agent (and simulator) with analysis tools
    Update simulator to return some values that are
    pertaining to each simulator run (100 trials of
    the agent). The values are then returned to
    agent.py, in which the main run() function is
    called with several alpha, epsilon and gamma
    parameters. In agent.py, the run(), main entry,
    are all made to support both running the project
    in "default" mode or to run it in analysis mode. Additionally make the simulations multi-process
    as larger parameter spaces seem to take quite
    a while.
    
    Also the learning factor (alpha_prime) is added
    to the agent.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -2,6 +2,10 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import pandas as pd
+import numpy as np
+from multiprocessing import Pool
+import sys
 
 class QLearner(object):
     """ 
@@ -19,7 +23,7 @@ class QLearner(object):
         total_num_states = 1
         for attribute in self.state_space.iterkeys():
             total_num_states *= len(self.state_space[attribute])
-        print "Q-Learner has {} possible states and {} actions".format(total_num_states, len(self.action_space))
+        #print "Q-Learner has {} possible states and {} actions".format(total_num_states, len(self.action_space))
 
     def optimal_action(self, state):
         q = []
@@ -66,7 +70,8 @@ class LearningAgent(Agent):
         'light': ['green', 'red']
     }
 
-    def __init__(self, env, exploration_factor=0.8, discount_factor=0.8):
+    def __init__(self, env, exploration_factor=0.8, discount_factor=0.8, learning_factor=1.0):        
+        assert learning_factor > 0. and learning_factor < 500., "Invalid learning factor!"
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
@@ -75,13 +80,16 @@ class LearningAgent(Agent):
         self.exploration_factor = exploration_factor
         self.qlearner = QLearner(self.state_space, self.allowed_actions, discount_factor)
         self.cumulative_reward = 0
-        self.exploration_amount = 1.0 # Starts with 1.0
+        self.n_penalties = 0
+        self.exploration_amount = 1.0 # Starts with 1.0        
+        self.learning_factor = learning_factor
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
 
         # Prepare for a new trip; reset any variables here, if required
         self.cumulative_reward = 0
+        self.n_penalties = 0
         self.exploration_amount *= self.exploration_factor # Decays exponentially every iteration
 
     def update(self, t):
@@ -118,30 +126,117 @@ class LearningAgent(Agent):
         }
 
         # Learn policy based on state, action, reward
-        self.qlearner.update(self.state, action, reward, state_after_action, 1./(t+1))
+        self.qlearner.update(self.state, action, reward, state_after_action, self.learning_factor/(self.learning_factor+t+1))
         
         # Update other variables for debug
         self.cumulative_reward += reward
+        if reward < 0:
+            self.n_penalties += 1
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
-def run():
+def run(epsilon = 0.8, gamma = 0.8, alpha_prime=1.0, update_delay=0.05, display=True):
     """Run the agent for a finite number of trials."""
 
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
+    a = e.create_agent(LearningAgent, exploration_factor=epsilon, discount_factor=gamma, learning_factor=alpha_prime)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.05, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=update_delay, display=display)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=100)  # run for a specified number of trials
+    stats = sim.run(n_trials=100)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
+    return stats
+
+def unpack_run(arg):
+    return run(*arg)
+
+def analyze(num_sim_runs=100):
+    """Run simulation multiple times with different parameters"""
+    
+    # Create a DataFrame to store all the analysis info
+    analysis = pd.DataFrame(
+        columns=[
+            'exploration_rate',
+            'discount_factor',
+            'learning_factor',
+            'mean_normed_time_left',
+            'std_normed_time_left',
+            'mean_normed_n_penalties',
+            'std_normed_n_penalties',
+            'mean_normed_cumulative_reward',
+            'std_normed_cumulative_reward',
+            'dest_reached_rate']
+            )
+
+    count = 0
+
+    for epsilon in np.linspace(0.1, 1.0, 40):
+        for gamma in np.linspace(0.1, 1.0, 40):
+            for alpha_prime in np.linspace(1.0, 8.0, 3):
+                stats = pd.DataFrame()
+
+                epsilon_arr = np.empty(num_sim_runs)
+                gamma_arr = np.empty(num_sim_runs)
+                alpha_prime_arr = np.empty(num_sim_runs)
+                update_delay_arr = np.zeros(num_sim_runs)
+                display_arr = np.empty(num_sim_runs, dtype=bool)
+                epsilon_arr.fill(epsilon)
+                gamma_arr.fill(gamma)
+                alpha_prime_arr.fill(alpha_prime)
+                display_arr.fill(False)
+
+                print "Measuring for epsilon {}, gamma {}, alpha_prime {}".format(epsilon, gamma, alpha_prime)
+
+                pool = Pool()
+                ret = pool.map(unpack_run, zip(epsilon_arr, gamma_arr, alpha_prime_arr, update_delay_arr, display_arr))
+                pool.close()
+                pool.join()
+
+                for ret_val in ret:
+                    stats = stats.append(ret_val)
+                
+                normed_time_left = stats['time_left'] / stats['deadline']
+                normed_n_penalties = stats['n_penalties'] / stats['deadline']
+                normed_cumulative_reward = stats['cumulative_reward'] / stats['deadline']
+                dest_reached_rate = float(stats['dest_reached'].value_counts()[True]) / stats['dest_reached'].shape[0]
+
+                mean_normed_time_left = np.mean(normed_time_left)
+                std_normed_time_left = np.std(normed_time_left)
+                mean_normed_n_penalties = np.mean(normed_n_penalties)
+                std_normed_n_penalties = np.std(normed_n_penalties)
+                mean_normed_cumulative_reward = np.mean(normed_cumulative_reward)
+                std_normed_cumulative_reward = np.std(normed_cumulative_reward)
+
+                count += 1
+
+                analysis.loc[count] = (
+                    epsilon, 
+                    gamma,
+                    alpha_prime, 
+                    mean_normed_time_left,
+                    std_normed_time_left, 
+                    mean_normed_n_penalties,
+                    std_normed_n_penalties,
+                    mean_normed_cumulative_reward,
+                    std_normed_cumulative_reward,
+                    dest_reached_rate
+                    )
+
+    print "Ran {} simulations with various parameters".format(count)
+    analysis.to_csv("analysis/analysis.csv")
 
 if __name__ == '__main__':
-    run()
+    # If no arguments supplied, "run" in default mode
+    if len(sys.argv) == 1:
+        run()
+    elif sys.argv[1] == 'analyze':
+        analyze(40)
+    else:
+        print "Invalid arguments!"
